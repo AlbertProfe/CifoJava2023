a20027f2bb0a3b145ae5032de9c2b753d72838ed
emotionysand <emotionysand@gmail.com>
2023-03-15 19:37:15 +0100

    #feat: fake login

--- LibraryManagement/src/main/java/com/example/myFirstSpring/controller/HomeController.java
+++ LibraryManagement/src/main/java/com/example/myFirstSpring/controller/HomeController.java
@@ -1,21 +1,45 @@
 package com.example.myFirstSpring.controller;
 
+
+import com.example.myFirstSpring.model.Librarian;
+import com.example.myFirstSpring.service.LibrarianService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.*;
+
+
 
 @Controller
 public class HomeController {
 
-    private final Set<String> sessionIds = Collections.synchronizedSet(new HashSet<>());
-    @RequestMapping("/home")
-    public String gethome(HttpSession session,Model model, HttpServletRequest request){
+    @Autowired
+    LibrarianService librarianService;
 
-       session.setAttribute("user", "user java object");
+    private final Set<String> sessionIds = Collections.synchronizedSet(new HashSet<>());
+
+    @RequestMapping("/login")
+    public String getLogin(Model model) {
+
+        model.addAttribute("librarians", librarianService.getAllLibrarians().values());
+
+        return "login";
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    public String getHome(HttpSession session,Model model, HttpServletRequest request,
+                     @RequestParam("librarianIdFromSelect") String librarianId){
+
+
+        session.setAttribute("librarianId", librarianId);
 
         model.addAttribute("todayDate", new Date().toString());
 
@@ -25,6 +49,8 @@ public class HomeController {
         model.addAttribute("sessionType", session.getClass().getName());
         model.addAttribute("sessionCount", this.sessionIds.size());
         model.addAttribute("requestCount", getRequestCount(session));
+
+
 
         Map<String, String> map = new HashMap<String, String>();
 
@@ -42,16 +68,18 @@ public class HomeController {
         return "home";
     }
 
+
+
     private Object getRequestCount(HttpSession session) {
 
-        Integer requestCount = (Integer) session.getAttribute("requestCount");
+    Integer requestCount = (Integer) session.getAttribute("requestCount");
 
-        requestCount = requestCount != null ? requestCount : 0;
-        requestCount++;
+    requestCount = requestCount != null ? requestCount : 0;
+    requestCount++;
 
-        session.setAttribute("requestCount", requestCount);
+    session.setAttribute("requestCount", requestCount);
 
-        return requestCount;
+    return requestCount;
     }
 
 
