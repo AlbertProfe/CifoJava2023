5538e59e988fc8ec07009644717f8ae820096916
emotionysand <emotionysand@gmail.com>
2023-03-16 19:56:40 +0100

    #feat: simplify login and logout

--- LibraryManagement/src/main/java/com/example/myFirstSpring/controller/HomeController.java
+++ LibraryManagement/src/main/java/com/example/myFirstSpring/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.myFirstSpring.controller;
 
-
-import com.example.myFirstSpring.model.Librarian;
 import com.example.myFirstSpring.service.LibrarianService;
 import com.example.myFirstSpring.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,15 +7,10 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 import java.util.*;
-
-
 
 @Controller
 public class HomeController {
@@ -28,51 +21,60 @@ public class HomeController {
     UserService userService;
     private final Set<String> sessionIds = Collections.synchronizedSet(new HashSet<>());
 
+
     @RequestMapping({"/","/login"})
-    public String getLogin() {
+    public String getLogin(Model model, HttpSession session) {
+        model.addAttribute("users", userService.getAllUsers().values());
+        model.addAttribute("librarians", librarianService.getAllLibrarians().values());
+
+        if ( session.getAttribute("isLogin") == null ) session.setAttribute("isLogin", false);
         return "login/login";
     }
 
-    @RequestMapping("/loginUser")
-    public String getLoginUser(Model model) {
-        model.addAttribute("users", userService.getAllUsers().values());
-        return "login/loginUser";
+    @RequestMapping("/home")
+    public String getHome(Model model) {
+        return "home";
     }
 
-    @RequestMapping("/loginLibrarian")
-    public String getLoginLibrarian(Model model) {
-        model.addAttribute("librarians", librarianService.getAllLibrarians().values());
-        return "login/loginLibrarian";
+    @RequestMapping("/logout")
+    public String logout(Model model, HttpSession session) {
+        session.invalidate();
+        return "redirect:login";
     }
+
 
     @RequestMapping(value = "/home", method = RequestMethod.POST)
-    public String getHome(HttpSession session,Model model, HttpServletRequest request,
+    public String login(HttpSession session, HttpServletRequest request,
                      @RequestParam("librarianIdFromSelect") String librarianId,
                     @RequestParam("userIdFromSelect") String userId){
 
-        session.setAttribute("librarianId", librarianId);
-        session.setAttribute("userId", userId);
-        session.setAttribute("session-creation-timestamp", new Date().toString());
-        model.addAttribute("todayDate", new Date().toString());
+        if (!((boolean) session.getAttribute("isLogin"))) {
+            this.sessionIds.add(session.getId());
 
-        this.sessionIds.add(session.getId());
+            session.setAttribute("librarianId", librarianId);
+            session.setAttribute("userId", userId);
+            session.setAttribute("session-creation-timestamp", new Date().toString());
+            session.setAttribute("todayDate", new Date().toString());
+            session.setAttribute("requestCount", getRequestCount(session));
+            session.setAttribute("sessionId", session.getId());
+            session.setAttribute("sessionCount", this.sessionIds.size());
 
-        model.addAttribute("sessionId", session.getId());
-        model.addAttribute("sessionType", session.getClass().getName());
-        model.addAttribute("sessionCount", this.sessionIds.size());
-        model.addAttribute("requestCount", getRequestCount(session));
+            Map<String, String> headerNamesMap = new HashMap<String, String>();
 
-        Map<String, String> map = new HashMap<String, String>();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String key = (String) headerNames.nextElement();
+                String value = request.getHeader(key);
+                headerNamesMap.put(key, value);
+            }
 
-        Enumeration headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String key = (String) headerNames.nextElement();
-            String value = request.getHeader(key);
-            map.put(key, value);
+            session.setAttribute("httpServletRequestHeaders", headerNamesMap);
+            session.setAttribute("isLogin", true);
+            return "redirect:home";
+        } else {
+            return "redirect:/";
         }
-        model.addAttribute("httpServletRequest", map);
 
-        return "home";
     }
 
     private Object getRequestCount(HttpSession session) {
